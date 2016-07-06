79b48cde9dcd9cc1fdec2955ec745a75d66f9c8c
Baxter Ross <baxterross@gmail.com>
2016-07-05 22:30:28 -0700

    fixing drag code

--- flow/main.js
+++ flow/main.js
@@ -18,27 +18,15 @@
 		gutterWidth = 20,
 		height, width;
 
-		flowContainer.interactive = true;
-		flowContainer
-        // events for drag start
-        .on('mousedown', onDragStart)
-        .on('touchstart', onDragStart)
-        // events for drag end
-        .on('mouseup', onDragEnd)
-        .on('mouseupoutside', onDragEnd)
-        .on('touchend', onDragEnd)
-        .on('touchendoutside', onDragEnd)
-        // events for drag move
-        .on('mousemove', onDragMove)
-        .on('touchmove', onDragMove);
 		stage.addChild(flowContainer);
+		attachDragEvents(flowContainer);
 
 		for (i = 0; i < 20; i++) {
 			texture = PIXI.Texture.fromImage('man.png'),
 			man = new PIXI.Sprite(texture);
 			man.height = getDimensions().height - gutterWidth;
 			man.width = man.height * 0.31;
-			man.anchor.x = man.anchor.y = 0.5;
+			man.anchor.set(0.5);
 			man.position.x = (man.width * 0.5) + (man.width * i) + (gutterWidth * i) + gutterWidth;
 			man.position.y = (man.height * 0.5) + (gutterWidth * 0.5);
 			flowContainer.addChild(man);
@@ -54,6 +42,22 @@
 	};
 
 	var onDragStart = function(event) {
+		this.startPosition = {
+			mouse: {
+				global: {
+					x: event.data.global.x,
+					y: event.data.global.y
+				},
+				relative: {
+					x: event.data.global.x - this.position.x,
+					y: event.data.global.y - this.position.y
+				}
+			},
+			element: {
+				x: this.position.x,
+				y: this.position.y
+			}
+		};
 		this.data = event.data;
 	    this.alpha = 0.5;
 		this.dragging = true;
@@ -65,36 +69,30 @@
 	};
 	var onDragMove = function(event) {
 		if (this.dragging) {
-			var newPosition = this.data.getLocalPosition(this.parent);
-			this.position.x = newPosition.x;
-			this.position.y = newPosition.y;
+			this.currentPosition = {
+				mouse: {
+					x: event.data.global.x,
+					y: event.data.global.y
+				}
+			};
+			this.movementSinceStart = {
+				x: this.currentPosition.mouse.x - this.startPosition.mouse.global.x,
+				y: this.currentPosition.mouse.y - this.startPosition.mouse.global.y
+			};
+			this.position.x = this.startPosition.element.x + this.movementSinceStart.x;
+			this.position.y = this.startPosition.element.y + this.movementSinceStart.y;
 	    }
 	};
-
-	window.addEventListener('keydown', function(event) {
-		var increment = 2,
-			element = window.flowContainer;
-
-		switch (event.keyCode) {
-			case 37: // left
-				element.position.x -= increment;
-			break;
-			case 39: // right
-				element.position.x += increment;
-			break;
-			case 38: // up
-				element.position.y -= increment;
-			break;
-			case 40: // down
-				element.position.y += increment;
-			break;
-			case 188: // "<"
-				element.rotation -= 0.1;
-			break;
-			case 190: // ">"
-				element.rotation += 0.1;
-			break;
-		}
-	});
+	var attachDragEvents = function(element) {
+		element.interactive = true;
+		element.on('mousedown', onDragStart)
+	        .on('touchstart', onDragStart)
+	        .on('mouseup', onDragEnd)
+	        .on('mouseupoutside', onDragEnd)
+	        .on('touchend', onDragEnd)
+	        .on('touchendoutside', onDragEnd)
+	        .on('mousemove', onDragMove)
+	        .on('touchmove', onDragMove);
+	};
 
 }(window));
